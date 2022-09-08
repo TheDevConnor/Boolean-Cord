3521013b096fc9aa2b2f67e2df49974a820e1703
Connor <99085769+TheRealHiThere@users.noreply.github.com>
2022-09-07 23:35:49 -0400

    did a bit of ui and some fun things a bit broken

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StreamChat } from 'stream-chat'
 import { Chat } from 'stream-chat-react'
 import Cookies from 'universal-cookie'
 
 import { ChannelListContainer, ChannelContainer, Auth } from './components'
 
+import 'stream-chat-react/dist/css/index.css'
 import './App.css'
 
 const cookies = new Cookies()
@@ -26,13 +27,28 @@ if (authToken) {
 }
 
 const App = () => {
-    if (!authToken) return <Auth />
+  const [createType, setCreateType] = useState('')
+  const [isCreating, setIsCreating] = useState(false)
+  const [isEditing, setIsEditing] = useState(false)
+
+  if (!authToken) return <Auth />
 
   return (
     <div className='app__wrapper'>
         <Chat client={client}>
-            <ChannelListContainer />
-            <ChannelContainer />
+            <ChannelListContainer 
+                isCreating={isCreating}
+                setIsCreating={setIsCreating}
+                setCreateType={setCreateType}
+                setIsEditing={setIsEditing}
+            />
+            <ChannelContainer
+                isCreating={isCreating}
+                setIsCreating={setIsCreating}
+                setIsEditing={setIsEditing}
+                isEditing={isEditing}
+                createType={createType}
+            />
         </Chat>
     </div>
   )
